8cf4599fabc4bd9f791f156c543e31b34492ac2a
imma <immamugambi@gmail.com>
2018-05-21 18:46:14 +0300

    Securing password through hashing

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 class User(db.Model): #create 'User' class to help in creating new users
    
@@ -6,10 +8,27 @@ class User(db.Model): #create 'User' class to help in creating new users
     id = db.Column(db.Integer,primary_key = True) #create columns using db.Column class which represents a single column.db.Integer specifies data to be stored
     username = db.Column(db.String(255)) #db.String class specifies data to be a string with 255 characters maximum
     email = db.Column(db.String)
-    password = db.Column(db.String)
+    password_hash = db.Column(db.String)
+    pass_secure = db.Column(db.String(255))
+
+
+    pass_secure  = db.Column(db.String(255))
+
+        @property #@property decorator creates a write only class property 'password'
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
 
     def __repr__(self): #repr method easens debugging of our application
         return f'User {self.username}'
+
 
 class Pitch(db.Model):
     #List of pitches
@@ -20,9 +39,10 @@ class Pitch(db.Model):
     title = db.Column(db.String(255))
     comment = db.Column(db.String)
     category = db.Column(db.String)
-    user_id = db.Column(db.String, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     vote = db.Column(db.Integer)
     users = db.relationship('User', backref='pitch', lazy='dynamic')
+
 
 class Comment(db.Model):
     #User comments
@@ -31,6 +51,6 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     feedback = db.column(db.String)
-    user_id = db.Column(db.String(255), db.ForeignKey('user.id'))
-    time_posted = db.Column(db.dateTime)
-    pitches = db.relationship('Pitch', backref='comment' lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time_posted = db.Column(db.DateTime)
+    pitches = db.relationship('Pitch', backref='comment', lazy='dynamic')
