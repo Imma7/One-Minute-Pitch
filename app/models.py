504282be8bfd94304e0e308cc6e5a3b2220fb66a
imma <immamugambi@gmail.com>
2018-05-21 15:14:41 +0300

    Create User class

--- app/models.py
+++ app/models.py
@@ -1,18 +1,11 @@
-from . import db 
+from . import db
 
 class User(db.Model): #create 'User' class to help in creating new users
     __tablename__ = 'users' #__tablename__ variable allows us to give the tables in our db proper names
     id = db.Column(db.Integer,primary_key = True) #create columns using db.Column class which represents a single column.db.Integer specifies data to be stored
     username = db.Column(db.String(255)) #db.String class specifies data to be a string with 255 characters maximum
+    email = db.Column(db.String)
+    password = db.Column(db.String)
 
     def __repr__(self): #repr method easens debugging of our application
         return f'User {self.username}'
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-
-    id = db.column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-
-    def __repr__(self):
-        return f'User {self.name}'
